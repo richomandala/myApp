6cf3e93d8173dcc85e53cc27b73ed1e477ab9ea4
richomandala <richomandala12@gmail.com>
2019-11-01 08:00:47 +0700

    firstApp

---
+++ src/app/calculator/calculator.page.ts
@@ -0,0 +1,162 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.page.html',
+  styleUrls: ['./calculator.page.scss'],
+})
+export class CalculatorPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  preVal = "";
+  val = "0";
+  newVal = "0";
+
+  zero() {
+    if (parseInt(this.val) > 0 || this.val.search(".") > 0) {
+      this.val = this.val + "0";
+    }
+  }
+
+  one() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "1";
+    }
+    else {
+      this.val = "1";
+    }
+  }
+
+  two() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "2";
+    }
+    else {
+      this.val = "2";
+    }
+  }
+
+  three() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "3";
+    }
+    else {
+      this.val = "3";
+    }
+  }
+
+  four() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "4";
+    }
+    else {
+      this.val = "4";
+    }
+  }
+
+  five() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "5";
+    }
+    else {
+      this.val = "5";
+    }
+  }
+
+  six() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "6";
+    }
+    else {
+      this.val = "6";
+    }
+  }
+
+  seven() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "7";
+    }
+    else {
+      this.val = "7";
+    }
+  }
+
+  eight() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "8";
+    }
+    else {
+      this.val = "8";
+    }
+  }
+
+  nine() {
+    if (parseFloat(this.val) > 0 || this.val.slice(this.val.length-1,this.val.length) == ".") {
+      this.val = this.val + "9";
+    }
+    else {
+      this.val = "9";
+    }
+  }
+
+  comma() {
+    if (this.val.search(".") < 1) {
+      this.val = this.val + ".";
+    }
+  }
+
+  add() {
+    if (parseFloat(this.val) > 0) {
+      this.val = this.val + "+";
+    }
+  }
+
+  sub() {
+    if (parseFloat(this.val) > 0) {
+      this.val = this.val + "-";
+    }
+  }
+
+  mul() {
+    if (parseFloat(this.val) > 0) {
+      this.val = this.val + "*";
+    }
+  }
+
+  div() {
+    if (parseFloat(this.val) > 0) {
+      this.val = this.val + "/";
+    }
+  }
+
+  rem() {
+    if (this.val == "Kesalahan") {
+      this.val = "0";
+    }
+    else if (this.val.length > 1) {
+      this.val = this.val.slice(0, this.val.length - 1);
+    }
+    else {
+      this.val = "0";
+    }
+  }
+
+  clear() {
+    this.val = "0";
+  }
+
+  result() {
+    try {
+      this.preVal = this.val;
+      this.newVal = eval(this.val);
+      this.val = this.newVal;
+    } catch (error) {
+      this.val = "Kesalahan"
+    }
+  }
+
+}
